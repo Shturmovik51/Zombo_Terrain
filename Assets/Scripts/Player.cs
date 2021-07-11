67b8a81d52e2c37c8f85bec00781b3f5ffb50267
Shturmovik51 <Shturmovik51@gmail.com>
2021-07-11 18:28:40 +0300

    Waterfall

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CharacterController charControl;
+    [SerializeField] private Transform groundDetector;
+    [SerializeField] private Transform head;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravityForse;
+    [SerializeField] private int playerSpeed;
+    [SerializeField] private int sensitivity;
+    [SerializeField] private int jumpForse;
+
+    private Vector3 gravitation;
+    private Vector3 oldPos;
+    private Vector3 newPos;
+    private float xRotation;
+    private bool isGrounded;
+    private int shiftedSpeed;
+    private int playerVelosity;
+
+    void Start()
+    {
+        isGrounded = false;
+        xRotation = 0f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+        PlayerLook();
+    }
+
+    private void PlayerMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        oldPos = transform.position;
+        isGrounded = Physics.CheckSphere(groundDetector.position, 0.3f, groundMask);
+
+        shiftedSpeed = 0;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            shiftedSpeed = playerSpeed;
+        }
+
+        var moveDirection = transform.right * x + transform.forward * z;
+
+        if (isGrounded)
+        {
+            gravitation.y = -2f;
+        }
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            gravitation.y = Mathf.Sqrt(jumpForse * -2 * gravityForse);
+        }
+
+        gravitation.y += gravityForse * Time.deltaTime;
+
+        charControl.Move(moveDirection * (playerSpeed + shiftedSpeed) * Time.deltaTime);
+        charControl.Move(gravitation * Time.deltaTime);
+
+        newPos = transform.position;
+        playerVelosity = (int)(Vector3.Magnitude(newPos - oldPos) / Time.deltaTime);
+    }
+
+    private void PlayerLook()
+    {
+        var mouseLookX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        var mouseLookY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        
+        xRotation -= mouseLookY;
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+        transform.Rotate(0f, mouseLookX, 0f);
+        head.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
