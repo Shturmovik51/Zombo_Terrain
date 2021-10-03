bd3eca7017ebf2f2a5280e50ad28c9669e15dc14
Shturmovik51 <Shturmovik51@gmail.com>
2021-10-03 03:31:57 +0300

    HW_7

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+namespace HomeWork
+{    public static class Extensions
+    {
+        public static int HowMuchChars(this string self)
+        {
+            return self.Length;
+        }
+
+        public static int HowMuchParameters<T>(this List<T> self)
+        {
+            return self.Count;                
+        }
+
+        public static void LogElementsCount<T>(this List<T> self)
+        {            
+            var list = from parameter in self
+                    group parameter by parameter into groups
+                    let count = groups.Count()
+                    select new { groups.Key, count};
+
+            foreach (var item in list)
+            {
+                Debug.Log($"{item.Key} - встречается {item.count} раз(а)");
+            }
+        }  
+    }
+}
+
